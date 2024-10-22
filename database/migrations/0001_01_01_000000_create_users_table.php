5be74bd8749da9ebbc96d0608544ab5e73f919c5
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-22 13:54:44 +0200

    Added uuid support to user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('username', 32)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
