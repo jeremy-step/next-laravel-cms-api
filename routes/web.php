d1433e05c995ce41c6b174fc767583cdb3a6ee85
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-22 13:46:11 +0200

    Defined front routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,48 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\FrontRoutesMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/reset-password', function () {
-    return "Reset Password";
-})->name('password.reset');
+require_once __DIR__.'/fortify.php';
 
-Route::get('/{foo?}', fn() => abort(400))->where(['foo' => '.*']);
+Route::name('front.')
+    ->middleware(FrontRoutesMiddleware::class)
+    ->group(function (): void {
+
+        Route::post('/api/set-cookies')->name('set.cookies');
+
+        Route::name('cp.')
+            ->prefix('/control-panel')
+            ->group(function (): void {
+
+                Route::get('/dashboard')->name('dashboard.index');
+
+                Route::name('pages.')
+                    ->prefix('/pages')
+                    ->group(function (): void {
+
+                        Route::get('/pages')->name('index');
+                        Route::get('/pages/create')->name('create');
+                        Route::get('/pages/{pageId}')->whereUuid('pageId')->name('edit');
+
+                    });
+
+                Route::get('/settings')->name('settings.index');
+
+            });
+
+        Route::get('/login')->name('login');
+        Route::get('/register')->name('register');
+
+        Route::get('/{permalink?}')->where(['permalink' => '.+'])->name('page.permalink');
+
+    });
+
+Route::middleware(FrontRoutesMiddleware::class)->group(function (): void {
+
+    Route::get('/email/verify')->name('verification.notice');
+
+});
+
+Route::fallback(fn () => abort(400));
