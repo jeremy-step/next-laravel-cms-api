5dc4fc8f531db223c838e2901f3a23c48ed6fe5f
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-22 13:39:34 +0200

    Initial tests

---
+++ tests/Feature/RoutesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class RoutesTest extends TestCase
+{
+    public function test_front_and_any_non_specific_route_return_bad_request_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(400);
+
+        $response = $this->get('/'.Str::random());
+
+        $response->assertStatus(400);
+    }
+}
