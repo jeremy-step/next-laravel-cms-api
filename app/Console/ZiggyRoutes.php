066c1ee73d693a1f9e5806539aed4c27c8ba0c18
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-23 22:47:50 +0200

    Custom ziggy config generator to unclude middleware and uri patterns

---
+++ app/Console/ZiggyRoutes.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console;
+
+use Illuminate\Support\Facades\Route;
+use Stringable;
+use Tighten\Ziggy\Ziggy;
+
+class ZiggyRoutes implements Stringable
+{
+    public function __construct(
+        protected Ziggy $ziggy,
+    ) {}
+
+    public function __toString(): string
+    {
+        $routes = Route::getRoutes()->getRoutesByName();
+        $ziggy = collect($this->ziggy->toArray());
+        $ziggyRoutes = collect($ziggy['routes'] ?? []);
+        
+        $ziggy['routes'] = $ziggyRoutes->map(function ($route, $name) use ($routes): array {
+            $middleware = array_filter(
+                $routes[$name]->middleware(),
+                fn ($middleware) => str_starts_with($middleware, 'front')
+            );
+            
+            if (!empty($middleware)) {
+                $route['middleware'] = array_values($middleware);
+            }
+
+            $route['pattern'] = '/' . ltrim(
+                preg_replace_callback(
+                    "/{(([^{}]+?)\?|([^{}]+))}/",
+
+                    function ($matches) use (&$route): string {
+                        $regex = "";
+
+                        if (isset($route['wheres']) && isset($matches[2])) {
+                            $includeRegex = $route['wheres']['_' . ($matches[3] ?? $matches[2])] ?? false;
+                            
+                            if ($includeRegex) {
+                                $regex = $route['wheres'][$matches[3] ?? $matches[2]] ?? null;
+                                $regex = $regex ? "($regex)" : "";
+
+                                unset($route['wheres']['_' . ($matches[3] ?? $matches[2])]);
+                            }
+                        }
+
+                        return ":{$matches[1]}$regex";
+                    },
+                    
+                    $route['uri']
+                )
+            , '/');
+
+            return $route;
+        });
+
+        return <<<JAVASCRIPT
+        const Ziggy = {$ziggy->toJson()};
+        export { Ziggy };
+
+        JAVASCRIPT;
+    }
+}
