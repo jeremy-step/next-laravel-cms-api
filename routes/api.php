169e357318b7b368648ec4154e7a6602fa539cb1
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 09:27:15 +0100

    Return user with formatted data

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Helpers\UserHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,11 +11,42 @@ Route::name('api.')->group(function (): void {
 
         Route::get('/user', function (Request $request): array {
 
-            return ['data' => $request->user()];
+            $user = $request->user();
+
+            $userData = $user ? [
+                'id' => $user->id,
+                'username' => $user->username,
+                'email' => $user->email,
+                'emailVerifiedAt' => $user->email_verified_at,
+                'createdAt' => $user->created_at,
+                'updatedAt' => $user->updated_at,
+                'nameDisplay' => $user->name_display,
+                'nameFirst' => $user->name_first,
+                'nameSecond' => $user->name_second,
+                'nameLast' => $user->name_last,
+                'phone' => $user->phone,
+                'phonePrefix' => $user->phone_prefix,
+                'locale' => $user->locale,
+            ] : null;
+
+            if ($userData) {
+                UserHelper::formatNameDisplay($userData);
+                UserHelper::formatPhoneNumber($userData);
+            }
+
+            return ['data' => $userData];
 
         })->name('user');
 
+        Route::get('/authenticated', function (Request $request): array {
+
+            return ['data' => ['authenticated' => (bool) $request->user()]];
+
+        })->name('authenticated');
+
         Route::get('/test', fn () => response()->json('test'))->name('test');
+
+        Route::post('/test', fn () => '1')->name('ptest');
 
     });
 });
