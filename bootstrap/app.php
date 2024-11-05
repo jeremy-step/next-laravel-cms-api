3b1514015335657bf6b20dc9e25a82743daaf02b
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-05 11:40:24 +0100

    Always return JSON response from API requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\ApiJsonResponseMiddleware;
 use App\Http\Middleware\FrontPlaceholderMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->api(prepend: ApiJsonResponseMiddleware::class);
+
         $middleware->alias([
             'front' => FrontPlaceholderMiddleware::class,
             'front:guest' => FrontPlaceholderMiddleware::class,
@@ -23,5 +28,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e): bool {
+            if (str_starts_with(Route::currentRouteName() ?? '', 'api.')) {
+                return true;
+            }
+
+            return $request->expectsJson();
+        });
     })->create();
