7e45ed96afa2af369da03722984df52ff2349f1a
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-05 11:35:04 +0100

    Always return JSON response from API requests

---
+++ app/Http/Middleware/ApiJsonResponseMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiJsonResponseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Content-Type', 'application/json');
+
+        return $next($request);
+    }
+}
