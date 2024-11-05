f9e1c3ae885b0868fbc6d10790e2d2e5cf535509
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-05 11:40:38 +0100

    Changes to API routes

--- routes/api.php
+++ routes/api.php
@@ -2,51 +2,47 @@
 
 declare(strict_types=1);
 
-use App\Helpers\UserHelper;
+use App\Http\Controllers\PageController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function (): void {
+
     Route::middleware('auth:sanctum')->group(function (): void {
 
-        Route::get('/user', function (Request $request): array {
+        // --- AUTH ---
 
-            $user = $request->user();
+        Route::get('/user', function (Request $request): UserResource {
 
-            $userData = $user ? [
-                'id' => $user->id,
-                'username' => $user->username,
-                'email' => $user->email,
-                'emailVerifiedAt' => $user->email_verified_at,
-                'createdAt' => $user->created_at,
-                'updatedAt' => $user->updated_at,
-                'nameDisplay' => $user->name_display,
-                'nameFirst' => $user->name_first,
-                'nameSecond' => $user->name_second,
-                'nameLast' => $user->name_last,
-                'phone' => $user->phone,
-                'phonePrefix' => $user->phone_prefix,
-                'locale' => $user->locale,
-            ] : null;
-
-            if ($userData) {
-                UserHelper::formatNameDisplay($userData);
-                UserHelper::formatPhoneNumber($userData);
-            }
-
-            return ['data' => $userData];
+            return new UserResource($request->user());
 
         })->name('user');
 
-        Route::get('/authenticated', function (Request $request): array {
+        Route::get('/authenticated', fn () => null)->name('authenticated');
 
-            return ['data' => ['authenticated' => (bool) $request->user()]];
+        // --- PAGES ---
 
-        })->name('authenticated');
+        Route::name('pages.')->controller(PageController::class)->group(function (): void {
 
-        Route::get('/test', fn () => response()->json('test'))->name('test');
+            Route::post('/pages', 'store')->name('store');
 
-        Route::post('/test', fn () => '1')->name('ptest');
+            Route::patch('/pages/{page}', 'update')->name('update');
+
+            Route::delete('/pages/{page}', 'destroy')->name('destroy');
+
+        });
 
     });
+
+    // --- PAGES ---
+
+    Route::name('pages.')->controller(PageController::class)->group(function (): void {
+
+        Route::get('/pages', 'index')->name('index');
+
+        Route::get('/pages/{id}', 'get')->whereUuid('page')->name('get');
+
+    });
+
 });
