b744789854deb2a46fc25a38b34f6621f3b863ff
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-19 15:55:32 +0100

    Add trusted proxies config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/status',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
         $middleware->statefulApi();
         $middleware->api(prepend: ApiJsonResponseMiddleware::class);
 
