0e9d5dc1224c304a0d7656317a6160433e4ce725
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-30 01:23:20 +0100

    New default data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 
 use App\Models\Setting;
 use App\Models\User;
-use Date;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,33 +23,77 @@ class DatabaseSeeder extends Seeder
             'name_second' => 'Joe',
             'name_last' => 'Doe',
             'name_display' => '<first_name> <surname>',
+            'owner' => true,
         ]);
 
         $user->forceFill([
-            'email_verified_at' => Date::now(),
+            'email_verified_at' => now(),
         ])->save();
 
-        $page = $user?->pages()->create([
+        $frontPage = $user?->pages()->create([
             'permalink' => 'welcome',
-            'title' => 'Welcome',
-            'text' => 'My first page...',
+            'title' => 'Voluptates Numquam Autem Natus Dignissimos Aliquid Id Dolor Placeat Nam',
+            'text' => <<<'MD'
+            ## Est modi nisi cumque commodi
+
+            Lorem ipsum, dolor sit amet *consectetur adipisicing* elit. Vero **cupiditate at ullam** assumenda, ratione molestiae dolores laboriosam architecto iusto ea officiis nemo sint facere corporis. **Quisquam quidem soluta** aperiam vero in, alias expedita doloremque modi molestias hic magni consectetur ratione *reprehenderit* quos minus delectus, quaerat praesentium suscipit? Ab, cum autem!
+
+            - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, quidem.
+            - Cumque beatae mollitia aliquam eius doloremque architecto.
+            - Dolore delectus illo? Dignissimos repellat vitae eius totam.
+                - Perferendis officia, est modi nisi cumque commodi.
+            - Vel beatae voluptas hic eligendi suscipit in.
+
+            ---
+
+            ![Imagen](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg)
+
+            > Source: [Markdown](https://www.markdownguide.org/basic-syntax/#images-1)
+
+            ---
+
+            [GitHub Repo API](https://github.com/cima-alfa/next-laravel-cms-api)
+
+            [GitHub Repo Front](https://github.com/cima-alfa/next-laravel-cms-front)
+            MD,
         ]);
 
-        $user?->pages()->create([
+        $frontPage?->metadata()->create(['title' => 'Voluptates Numquam Autem Natus Dignissimos Aliquid Id Dolor Placeat Nam', 'description' => 'Meta description...', 'robots' => 'index,follow']);
+
+        $page = $user?->pages()->create([
             'permalink' => 'about',
             'title' => 'About',
             'text' => 'About page...',
         ]);
 
-        $user?->pages()->create([
+        $page?->metadata()->create(['title' => 'About', 'description' => 'Meta description...', 'robots' => 'index,follow']);
+
+        $page = $user?->pages()->create([
             'permalink' => 'contact',
             'title' => 'Contact',
             'text' => 'Contact page...',
         ]);
 
+        $page?->metadata()->create(['title' => 'Contact', 'description' => 'Meta description...', 'robots' => 'noindex,nofollow']);
+
         Setting::create([
-            'key' => 'frontpage',
-            'value' => $page->id,
+            'key' => 'general.website_name',
+            'value' => config('app.name'),
+        ]);
+
+        Setting::create([
+            'key' => 'general.frontpage',
+            'value' => $frontPage->id,
+        ]);
+
+        Setting::create([
+            'key' => 'mail.mailers.default.transport',
+            'value' => 'log',
+        ]);
+
+        Setting::create([
+            'key' => 'mail.mailers.default.from.email',
+            'value' => 'admin@example.dev',
         ]);
     }
 }
