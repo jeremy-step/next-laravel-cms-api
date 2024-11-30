e04cf1cf54a8147e8adc45296f65d83fbae4ae6a
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-30 01:24:18 +0100

    Common test functionality

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Database\Seeders\TestSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    /**
+     * Run a specific seeder before each test.
+     *
+     * @var string
+     */
+    protected $seeder = TestSeeder::class;
+
+    protected function autheticate(): void
+    {
+        $response = $this->json('post', route('login.store'), ['login' => 'admin', 'password' => 'admin']);
+
+        $response->assertOk();
+    }
 }
