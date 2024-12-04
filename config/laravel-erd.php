bdc682ca163b2846534d632df903845eb7c465cb
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-12-04 16:26:25 +0100

    DB diagrams config

---
+++ config/laravel-erd.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'binary' => [
+        'erd-go' => storage_path('app/private/erd/go'),
+        'dot' => storage_path('app/private/erd/dot'),
+    ],
+];
